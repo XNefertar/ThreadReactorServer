ef7c989a63b822147abc2fd6b0cc9fb2366a30be
XNefertar <xnefertar@gmail.com>
2025-03-08 13:41:02 +0800

    Update_LinuxTimer

--- TestCode/LinuxTimerDemo.cc
+++ TestCode/LinuxTimerDemo.cc
@@ -22,8 +22,10 @@ int main()
     // 设置定时器
     struct itimerspec new_value;
     // 设置定时器首次超时时间和之后的超时时间
+    // 首次超时时间
     new_value.it_value.tv_sec = 3;
     new_value.it_value.tv_nsec = 0;
+    // 之后的超时时间
     new_value.it_interval.tv_sec = 1;
     new_value.it_interval.tv_nsec = 0;
 
@@ -34,6 +36,8 @@ int main()
         return -1;
     }
 
+    // 初始化 select
+    // 设置所需监视的文件描述符
     fd_set readfds;
     FD_ZERO(&readfds);
     FD_SET(timerfd, &readfds);
@@ -41,6 +45,7 @@ int main()
     time_t t = time(NULL);
     while (true)
     {
+        // select 监视对应的文件描述符
         int ret = select(timerfd + 1, &readfds, NULL, NULL, NULL);
         if (ret == -1)
         {
@@ -57,12 +62,14 @@ int main()
         {
             uint64_t exp;
             ssize_t s = read(timerfd, &exp, sizeof(uint64_t));
-            if (s != sizeof(uint64_t))
+            if (s == -1)
             {
                 std::cerr << "read error" << std::endl;
                 return -1;
             }
 
+            // ctime 将时间值转换为可读的字符串格式
+            // 将 time_t类型的时间值转换为一个表示本地时间的字符串
             std::string str = ctime(&t);
             str[str.size() - 1] = '\0';
             std::cout << str << " timerfd timeout" << std::endl;
